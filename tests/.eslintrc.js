29e5f336e87138e3e97d9820b50cc15a6ae7eb53
RealDolos <dolos@cock.li>
2018-05-03 09:10:11 +0200

    .eslinting

--- tests/.eslintrc.js
+++ tests/.eslintrc.js
@@ -1,9 +1,9 @@
 module.exports = {
-    "env": {
-        "es6": true,
-        "mocha": true,
-        "node": true,
-        "commonjs": true,
-    },
-    "extends": "../.eslintrc.js",
+  env: {
+    es6: true,
+    mocha: true,
+    node: true,
+    commonjs: true,
+  },
+  extends: "../.eslintrc.js",
 };
